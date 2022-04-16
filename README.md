2b9c2e51962a1dc4d1a672483327990d9c0bf567
touchren <touchren@gmail.com>
2022-04-16 11:24:40 +0800

    增加日志打印

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # meituanmaicai
 美团买菜Autojs,在自动下单的基础上,增加捡漏的功能
+
+参考代码:
+* autojs教程 http://keep.ipromiseyourlife.com/2019/06/27/auto-js%E7%9A%84%E4%BD%BF%E7%94%A8%E4%BB%A5%E5%8F%8A%E8%96%85%E8%85%BE%E8%AE%AF%E7%BE%8A%E6%AF%9B%E6%8F%92%E4%BB%B6%E7%A4%BA%E4%BE%8B/
+* 下拉刷新代码参考 https://www.d1blog.com/autojs/1752.html
+* 核心代码来自 https://gist.github.com/zgl018/742b661edd2902e80db4bfca59d01e1a
+
+已知问题:
+* 到了支付页面不会自动点击`立即支付`
--- meituanmaicai.js
+++ meituanmaicai.js
@@ -5,102 +5,109 @@
  * @param {终点x} ex
  * @param {终点y} ey
  */
- function randomSwipe(sx, sy, ex, ey) {
-    //设置随机滑动时长范围
-    var timeMin = 300
-    var timeMax = 800
-    //设置控制点极限距离
-    var leaveHeightLength = 500
+function randomSwipe(sx, sy, ex, ey) {
+  //设置随机滑动时长范围
+  var timeMin = 300;
+  var timeMax = 800;
+  //设置控制点极限距离
+  var leaveHeightLength = 500;
 
-    //根据偏差距离，应用不同的随机方式
-    if (Math.abs(ex - sx) > Math.abs(ey - sy)) {
-        var my = (sy + ey) / 2
-        var y2 = my + random(0, leaveHeightLength)
-        var y3 = my - random(0, leaveHeightLength)
+  //根据偏差距离，应用不同的随机方式
+  if (Math.abs(ex - sx) > Math.abs(ey - sy)) {
+    var my = (sy + ey) / 2;
+    var y2 = my + random(0, leaveHeightLength);
+    var y3 = my - random(0, leaveHeightLength);
 
-        var lx = (sx - ex) / 3
-        if (lx < 0) { lx = -lx }
-        var x2 = sx + lx / 2 + random(0, lx)
-        var x3 = sx + lx + lx / 2 + random(0, lx)
-    } else {
-        var mx = (sx + ex) / 2
-        var x2 = mx + random(0, leaveHeightLength)
-        var x3 = mx - random(0, leaveHeightLength)
+    var lx = (sx - ex) / 3;
+    if (lx < 0) {
+      lx = -lx;
+    }
+    var x2 = sx + lx / 2 + random(0, lx);
+    var x3 = sx + lx + lx / 2 + random(0, lx);
+  } else {
+    var mx = (sx + ex) / 2;
+    var x2 = mx + random(0, leaveHeightLength);
+    var x3 = mx - random(0, leaveHeightLength);
 
-        var ly = (sy - ey) / 3
-        if (ly < 0) { ly = -ly }
-        var y2 = sy + ly / 2 + random(0, ly)
-        var y3 = sy + ly + ly / 2 + random(0, ly)
-    }  
+    var ly = (sy - ey) / 3;
+    if (ly < 0) {
+      ly = -ly;
+    }
+    var y2 = sy + ly / 2 + random(0, ly);
+    var y3 = sy + ly + ly / 2 + random(0, ly);
+  }
 
-    //获取运行轨迹，及参数
-    var time = [0, random(timeMin, timeMax)]
-    var track = bezierCreate(sx, sy, x2, y2, x3, y3, ex, ey)
+  //获取运行轨迹，及参数
+  var time = [0, random(timeMin, timeMax)];
+  var track = bezierCreate(sx, sy, x2, y2, x3, y3, ex, ey);
 
-   
-    log("随机控制点A坐标：" + x2 + "," + y2)
-    log("随机控制点B坐标：" + x3 + "," + y3)
-    log("随机滑动时长：" + time[1])
-	//log("track" + track)
+  log("随机控制点A坐标：" + x2 + "," + y2);
+  log("随机控制点B坐标：" + x3 + "," + y3);
+  log("随机滑动时长：" + time[1]);
+  //log("track" + track)
 
-    //滑动
-    gestures(time.concat(track))
+  //滑动
+  gestures(time.concat(track));
 }
 /**
  * 计算滑动轨迹
  */
 function bezierCreate(x1, y1, x2, y2, x3, y3, x4, y4) {
-    //构建参数
-    var h = 100;
-    var cp = [{ x: x1, y: y1 + h }, { x: x2, y: y2 + h }, { x: x3, y: y3 + h }, { x: x4, y: y4 + h }];
-    var numberOfPoints = 100;
-    var curve = [];
-    var dt = 1.0 / (numberOfPoints - 1);
+  //构建参数
+  var h = 100;
+  var cp = [
+    { x: x1, y: y1 + h },
+    { x: x2, y: y2 + h },
+    { x: x3, y: y3 + h },
+    { x: x4, y: y4 + h },
+  ];
+  var numberOfPoints = 100;
+  var curve = [];
+  var dt = 1.0 / (numberOfPoints - 1);
 
-    //计算轨迹
-    for (var i = 0; i < numberOfPoints; i++) {
-        var ax, bx, cx;
-        var ay, by, cy;
-        var tSquared, tCubed;
-        var result_x, result_y;
+  //计算轨迹
+  for (var i = 0; i < numberOfPoints; i++) {
+    var ax, bx, cx;
+    var ay, by, cy;
+    var tSquared, tCubed;
+    var result_x, result_y;
 
-        cx = 3.0 * (cp[1].x - cp[0].x);
-        bx = 3.0 * (cp[2].x - cp[1].x) - cx;
-        ax = cp[3].x - cp[0].x - cx - bx;
-        cy = 3.0 * (cp[1].y - cp[0].y);
-        by = 3.0 * (cp[2].y - cp[1].y) - cy;
-        ay = cp[3].y - cp[0].y - cy - by;
+    cx = 3.0 * (cp[1].x - cp[0].x);
+    bx = 3.0 * (cp[2].x - cp[1].x) - cx;
+    ax = cp[3].x - cp[0].x - cx - bx;
+    cy = 3.0 * (cp[1].y - cp[0].y);
+    by = 3.0 * (cp[2].y - cp[1].y) - cy;
+    ay = cp[3].y - cp[0].y - cy - by;
 
-        var t = dt * i
-        tSquared = t * t;
-        tCubed = tSquared * t;
-        result_x = (ax * tCubed) + (bx * tSquared) + (cx * t) + cp[0].x;
-        result_y = (ay * tCubed) + (by * tSquared) + (cy * t) + cp[0].y;
-        curve[i] = {
-            x: result_x,
-            y: result_y
-        };
+    var t = dt * i;
+    tSquared = t * t;
+    tCubed = tSquared * t;
+    result_x = ax * tCubed + bx * tSquared + cx * t + cp[0].x;
+    result_y = ay * tCubed + by * tSquared + cy * t + cp[0].y;
+    curve[i] = {
+      x: result_x,
+      y: result_y,
+    };
+  }
+
+  //轨迹转路数组
+  var array = [];
+  for (var i = 0; i < curve.length; i++) {
+    try {
+      var j = i < 100 ? i : 199 - i;
+      xx = parseInt(curve[j].x);
+      yy = parseInt(Math.abs(100 - curve[j].y));
+    } catch (e) {
+      break;
     }
+    array.push([xx, yy]);
+  }
 
-    //轨迹转路数组
-    var array = [];
-    for (var i = 0; i < curve.length; i++) {
-        try {
-            var j = (i < 100) ? i : (199 - i);
-            xx = parseInt(curve[j].x)
-            yy = parseInt(Math.abs(100 - curve[j].y))
-        } catch (e) {
-            break
-        }
-        array.push([xx, yy])
-    }
-
-    return array
+  return array;
 }
 
-
 const musicNotify = () => {
-  const m = "/storage/emulated/0/Download/WeiXin/平凡之路.mp3";
+  const m = "/storage/emulated/0/Download/心如止水.mp3";
   media.playMusic(m);
   sleep(media.getMusicDuration());
 };
@@ -118,23 +125,23 @@ const to_mall_cart = () => {
   }
 };
 
-const to_mine = () => {
-  mine_btn = id("img_mine").findOne();
-  if (mine_btn) {
-    mine_btn.parent().click(); //btn上一级控件可点击
-    //var loc = id("img_shopping_cart").findOne().bounds();//1.匹配id寻找位置。
-    //click(loc.centerX(), loc.centerY());
-    log("已进入我的");
-  } else {
-    log("未找到我的按钮，退出");
-    exit;
-  }
-};
+// const to_mine = () => {
+//   mine_btn = id("img_mine").findOne();
+//   if (mine_btn) {
+//     mine_btn.parent().click(); //btn上一级控件可点击
+//     //var loc = id("img_shopping_cart").findOne().bounds();//1.匹配id寻找位置。
+//     //click(loc.centerX(), loc.centerY());
+//     log("已进入我的");
+//   } else {
+//     log("未找到我的按钮，退出");
+//     exit;
+//   }
+// };
 
 const reload_mall_cart = () => {
   // 切换标签页面
   log("重新加载购物车");
-  randomSwipe(550,700,600,1100);
+  randomSwipe(550, 700, 600, 1100);
   //to_mine();
   //sleep(5000);
   //to_mall_cart();
@@ -150,7 +157,6 @@ const pay = () => {
 
 const selectTime = (countT, status) => {
   //选择送达时间
-
   textStartsWith("送达时间").findOne().parent().click();
   var selectedTime = null;
   hourClock_unfilterd = textContains(":00").find();
@@ -186,6 +192,7 @@ const selectTime = (countT, status) => {
   }
 
   if (selectedTime != null) {
+    log("选择可用时间");
     selectedTime.parent().click();
     sleep(50);
 
@@ -203,12 +210,14 @@ const selectTime = (countT, status) => {
       pay();
     }
   } else {
+    log("没有可用时间段")
     countT = countT + 1;
     if (countT > 18000) {
       toast("抢菜选择时间失败");
       exit;
     }
     sleep(100);
+    log("开始重新选择时间")
     selectTime(countT, false);
   }
 };
@@ -220,21 +229,22 @@ const submit_order = (count) => {
     toast("抢菜第" + count + "次尝试");
   }
   //美团买菜 结算按钮无id
-  //log(textStartsWith("结算").exists());
-  
-  if (!textStartsWith("结算").exists()) {	
+  if (!textStartsWith("结算").exists()) {
     log("未找到结算按钮，刷新页面");
     reload_mall_cart();
     submit_order(count);
   } else {
-	log("开始结算")
-	let submit_btn = textStartsWith("结算").findOne();
+    log("开始结算");
+    let submit_btn = textStartsWith("结算").findOne();
     submit_btn.parent().click(); //结算按钮点击
 
     sleep(1200);
 
     let retry_button = textMatches(/(我知道了|返回购物车)/);
     if (retry_button.exists()) {
+      // 1. 配送运力已约满
+      // 2. 门店已打烊
+      // 3. 订单已约满
       retry_button.findOne().parent().click();
       sleep(300);
     } else {
@@ -243,9 +253,8 @@ const submit_order = (count) => {
         toast("跳过加购");
         textStartsWith("放弃机会").findOne().parent().click();
         selectTime(0, false);
-      } else if (textStartsWith("我知道了").exists()) {
-        log("配送运力已约满");
-        textStartsWith("我知道了").findOne().parent().click();
+      } else {
+        log("存在其他情况");
       }
     }
 
